480d3bc9d6f6c8d6ba6092741bfa41392a4393a6
EnoughKK <ksu8063@naver.com>
2022-06-19 05:10:44 +0900

    +bullet

---
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveBullet : MonoBehaviour
+{
+    public GameObject sparkEffect;
+    private void OnCollisionEnter(Collision coll)
+    {
+        if(coll.collider.CompareTag("BULLET"))
+        {
+            Instantiate(sparkEffect, coll.transform.position, Quaternion.identity);
+            Destroy(coll.gameObject);
+        }
+    }
+}
