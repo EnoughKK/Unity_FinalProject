a89334836f7e2d562bcf2dc7ba8059d889ec6021
EnoughKK <ksu8063@naver.com>
2022-06-20 03:10:59 +0900

    +enemy_animation

---
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterCtrl : MonoBehaviour
+{
+    private readonly int hashTrace = Animator.StringToHash("IsTrace");
+    private readonly int hashAttack = Animator.StringToHash("IsAttack");
+    private readonly int hashHit = Animator.StringToHash("Hit");
+    public enum State
+    {
+        IDLE,
+        TRACE,
+        ATTACK,
+        DIE
+    }
+    public State state = State.IDLE;
+    public float traceDist = 10.0f;
+    public float attackDist = 2.0f;
+    public bool isDie = false;
+
+    private Transform monsterTr;
+    private Transform playerTr;
+    private NavMeshAgent agent;
+    private Animator anim;
+    void Start()
+    {
+        monsterTr = GetComponent<Transform>();
+        playerTr = GameObject.FindWithTag("PLAYER").GetComponent<Transform>();
+
+        // NavMeshAgent 컴포넌트 할당
+        agent = GetComponent<NavMeshAgent>();
+
+        anim = GetComponent<Animator>();
+
+        StartCoroutine(CheckMonsterState());
+        StartCoroutine(MonsterAction());
+    }
+
+    IEnumerator CheckMonsterState()
+    {
+        while(!isDie)
+        {
+            yield return new WaitForSeconds(0.3f);
+            float distance = Vector3.Distance(playerTr.position, monsterTr.position);
+
+            if(distance <= attackDist)
+            {
+                state = State.ATTACK;
+            }
+            else if (distance <= traceDist)
+            {
+                state = State.TRACE;
+            }
+            else
+            {
+                state = State.IDLE;
+            }
+        }
+    }
+
+    IEnumerator MonsterAction()
+    {
+        while(!isDie)
+        {
+            switch (state)
+            {
+                case State.IDLE:
+                    agent.isStopped = true;
+                    anim.SetBool(hashTrace, false);
+                    break;
+
+                case State.TRACE:
+                    agent.SetDestination(playerTr.position);
+                    agent.isStopped = false;
+                    anim.SetBool(hashTrace, true);
+                    anim.SetBool(hashAttack, false);
+                    break;
+                case State.ATTACK:
+                    anim.SetBool(hashAttack, true);
+                    break;
+                case State.DIE:
+                    break;
+            }
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.CompareTag("BULLET"))
+        {
+            Destroy(coll.gameObject);
+            anim.SetTrigger(hashHit);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        // 추적 사정거리 표시
+        if(state == State.TRACE)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, traceDist);
+        }
+        // 공격 사정거리 표시
+        if (state == State.ATTACK)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, attackDist);
+        }
+    }
+}
